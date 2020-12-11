3e6a70ce721bfc6392ecc3ec0d7e89dac409c058
shwetank429 <shwetanks125@gmail.com>
2020-12-11 23:15:22 +0530

    first commit

---
+++ js/Food.js
@@ -0,0 +1,26 @@
+class Food{
+    constructor(){
+        this.image=loadImage("../images/Milk.png");
+        this.foodStock
+        this.lastFed
+    }
+
+    getFoodStock(){
+
+    }
+
+     updateFoodStock(){
+
+     }
+
+      deductFood(){
+
+      }
+
+      display(){
+      var x=80,y=100;
+      
+      imageMode(CENTER)
+      image(this.image,720,220,70,70)
+      }
+}
